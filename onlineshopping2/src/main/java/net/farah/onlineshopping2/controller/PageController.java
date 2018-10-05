1fa6c63db6e5e477e1310024892283f9d68d5c82
farahinani <farahinaniahmad@gmail.com>
2018-10-05 14:38:03 +0800

    tutorial 8 done !

--- onlineshopping2/src/main/java/net/farah/onlineshopping2/controller/PageController.java
+++ onlineshopping2/src/main/java/net/farah/onlineshopping2/controller/PageController.java
@@ -1,6 +1,7 @@
 package net.farah.onlineshopping2.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,5 +17,28 @@ public class PageController {
 		mv.addObject("greeting","Welcome to Spring Web MVC princess Para");
 		return mv;
 	}
+	
+//	@RequestMapping(value="/test")
+//	public ModelAndView test(@RequestParam(value="greeting", required=false)String greeting) {
+//		if(greeting == null) {
+//			greeting = "hello Para";
+//		}
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting",greeting);
+//		return mv;
+//		
+//	}
+	
+	@RequestMapping(value="/test/{greeting}")
+	public ModelAndView test(@PathVariable("greeting")String greeting) {
+		if(greeting == null) {
+			greeting = "hello Para";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting",greeting);
+		return mv;
+		
+	}
+	
 }
 
